c09cc8e14c28a8509582b5d8a7343d1a6684d26a
drk01 <WFkXSFepPQz7Ff>
2020-07-20 06:03:19 -0500

    Hola mundo desde Flask

--- tutorial/main.py
+++ tutorial/main.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>¡Hola mundo desde el servidor de Flask!</h1>'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
